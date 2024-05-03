b7625480e0d5d4ffdaccde0b0d50811e803d77d7
xmifunx <r.eustafjev@yandex.ru>
2024-05-03 07:13:00 +0400

    second commit tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -5,16 +5,16 @@ tab.forEach((element) => {
     element.addEventListener('click', function() {
         const tabActive = document.querySelectorAll('div#tabs1.tabs > div.tab__navigation > div.tab.tab_active');
         const tabActiveContent = document.querySelectorAll('div#tabs1.tabs > div.tab__contents > div.tab__content.tab__content_active');
-        
-        var i = tab.indexOf(element);
-        element.classList.add('tab_active');
-        tabContent[i].classList.add('tab__content_active');
-        
+
         if (tabActive.length != 0) {
             tabActive[0].classList.remove('tab_active');
         }
         if (tabActiveContent.length != 0) {
             tabActiveContent[0].classList.remove('tab__content_active');
         }
+
+        const i = tab.indexOf(element);
+        element.classList.add('tab_active');
+        tabContent[i].classList.add('tab__content_active');
     });
 });
